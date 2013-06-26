91d82a60e504072a758483b4c800c82ca225bff9
ken knerr <molybrea@gmail.com>
2013-06-25 22:41:46 -0300

    Create ebay.php

---
+++ ebay.php
@@ -0,0 +1,13 @@
+<?php
+/* ebay.php - Sell item interface to user and SOAP API interpreter for ebay.com
+- accepts specific input from user for listing an ebay item
+- uses SOAP to interface with ebay.com to create the listing
+- displays status of and fees for that listing
+
+Looks like this will be our first foray into OO coding. All of the SOAP components use XML so building and parsing will
+require a mix of OO and procedural. THis oughta be fun....
+
+
+*/
+
+?>
